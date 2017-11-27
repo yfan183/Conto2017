dabea74cdb34988b33b0a9c8a96d1fb0706ce7e6
Ivan Fan <wow-ivan@hotmail.com>
2017-11-27 12:55:44 +1200

    testing branch changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+//comment used to test branch changes
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
